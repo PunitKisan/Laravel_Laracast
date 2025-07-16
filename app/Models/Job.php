35bfd13216e6549e109e61e7557c01873de8cd04
PunitKisan <punitcode18@gmail.com>
2025-07-17 00:07:58 +0530

    Episode 9 pivot table and belongsToMany

--- app/Models/Job.php
+++ app/Models/Job.php
@@ -2,6 +2,7 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 /**
@@ -9,6 +10,17 @@ use Illuminate\Database\Eloquent\Model;
  */
 class Job extends Model
 {
+    use HasFactory;
     protected $table = 'job_listings';
     protected $fillable = ['title', 'salary'];
+
+    public function employer(): \Illuminate\Database\Eloquent\Relations\BelongsTo
+    {
+        return $this->belongsTo(Employer::class);
+    }
+
+    public function tags(): \Illuminate\Database\Eloquent\Relations\BelongsToMany
+    {
+        return $this->belongsToMany(Tag::class, foreignPivotKey: "job_listing_id");
+    }
 }
