1dcb8f9c3488f8662b2d9d6fed8f39e579ff5327
PunitKisan <punitcode18@gmail.com>
2025-09-03 23:46:50 +0530

    Episode 16 Forms and CSRF

--- routes/web.php
+++ routes/web.php
@@ -17,12 +17,26 @@ Route::get('/contact', function () {
 });
 
 Route::get('/jobs', function () {
-    $jobs = Job::with('employer')->paginate(10);
-    return view('jobs', [
+    $jobs = Job::with('employer')->latest()->paginate(10);
+    return view('jobs.index', [
         'jobs' => $jobs
     ]);
 });
 
+Route::get('/jobs/create', function () {
+    return view('jobs.create');
+});
+
 Route::get('/jobs/{id}', function ($id) {
-    return view('job', ['job' => Job::find($id)]);
+    return view('jobs.show', ['job' => Job::find($id)]);
+});
+
+Route::post('/jobs', function () {
+//    dd(request()->all());
+    //validation
+    Job::create([
+        'title' => request('title'),
+        'salary' => request('salary'),
+        'employer_id' => 1,
+    ]);
 });
